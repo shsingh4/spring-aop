9b598b2a576c427791aee80a93fae1364b12ed5d
shashank <shanky.nitc@gmail.com>
2021-05-14 21:58:55 -0400

    Initial Commit

---
+++ src/test/java/com/ultradev/springaop/SpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultradev.springaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
